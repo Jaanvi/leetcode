7a30f846a8b0bc617deb35213917238c20bf7d54
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2024-11-29 23:44:41 +0530

    Time: 3 ms (16.10%), Space: 23.3 MB (5.41%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        map<int, int> m; // Map to store occurrences of each number
+        int k = 0; // Count of unique elements
+        
+        for (int i = 0; i < nums.size(); i++) {
+            // Check if the element is not already in the map
+            if (m.find(nums[i]) == m.end()) {
+                nums[k++] = nums[i]; // Place the unique element in the array
+                m[nums[i]] = 2; // Mark it as seen in the map
+            }
+        }
+        
+        return k; // Return the count of unique elements
+    }
+};
