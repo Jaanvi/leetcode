8fc3f96fb045915f5e1bbea324b3af60769dbcd2
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2024-12-12 22:52:21 +0530

    Time: 6 ms (17.28%), Space: 12.9 MB (94.35%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,26 @@
+
+class Solution 
+{
+public:
+    long long pickGifts(vector<int>& gifts, int k) 
+    {
+        long long sum=0; 
+       for(int i=0;i<k;i++)
+       {
+           int maxindex=0;
+           for(int j=0;j<gifts.size();j++)
+           {
+               if(gifts[j]>=gifts[maxindex])
+               {
+                   maxindex=j;
+               }
+           }
+           gifts[maxindex] = static_cast<int>(sqrt(gifts[maxindex])); 
+       }  
+       for(int i=0;i<gifts.size();i++)
+       {
+           sum+=gifts[i];
+       }
+        return sum;
+    }
+};
