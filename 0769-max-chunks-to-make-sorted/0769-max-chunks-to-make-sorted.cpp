3d4e8454880c70e7fe80658464835b15393847b3
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2024-12-19 21:22:01 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (35.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -4,14 +4,28 @@ public:
     {
         int count=1;
         int maxi=0;
-        for(int i=0;i<arr.size()-1;i++)
-        {
-           maxi=max(maxi,arr[i]);
-           if(maxi==i)
-           {
-               count++;
-           }
-        }
+      for(int i=0;i<arr.size()-1;i++)
+      {
+          maxi=max(arr[i],maxi);
+          if(maxi==i)
+          {
+              count++;
+          }
+      }
         return count;
     }
 };
+
+
+
+//  int count=1;
+//         int maxi=0;
+//         for(int i=0;i<arr.size()-1;i++)
+//         {
+//            maxi=max(maxi,arr[i]);
+//            if(maxi==i)
+//            {
+//                count++;
+//            }
+//         }
+//         return count;
