0a089961e9fca1cc2f50da536bdefaf5f3c61359
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-02-21 16:15:57 +0530

    Time: 73 ms (46.05%), Space: 74.7 MB (50.74%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,21 +1,26 @@
+
+struct Compare {
+    bool operator()(vector<int>& pair1,vector<int>& pair2) {
+        int dist1 = pair1[0] * pair1[0] + pair1[1] * pair1[1];
+        int dist2 = pair2[0] * pair2[0] + pair2[1] * pair2[1];
+        return dist1 > dist2;
+    }
+};
+
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
-    {
-        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<>> pq;
-        vector<vector<int>>ans;
-        for(int i=0;i<points.size();i++)
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, Compare> pq(points.begin(),points.end());
+
+        vector<vector<int>> ans;
+        int count=0;
+        while(count<k)
         {
-            int dis = pow(points[i][0], 2) + pow(points[i][1], 2);
-            pq.push({dis,points[i]});
-        }
-        int count=1;
-        while(count<=k)
-        {
-            ans.push_back(pq.top().second);
+            ans.push_back(pq.top());
             pq.pop();
             count++;
         }
+
         return ans;
     }
 };
