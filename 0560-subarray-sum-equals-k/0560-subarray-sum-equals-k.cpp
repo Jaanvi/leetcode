18dd5ffead4ed9b38d0d43ba32dfdd5fcb41d512
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-06-25 12:55:03 +0530

    Time: 59 ms (43.5%), Space: 47.8 MB (31.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,14 +6,14 @@ public:
         map<int,int>m;
         int sum=0;
         int count=0;
-        // m[0]=1;//taki agr sum==k hai to hum
+        m[0]=1;//taki agr sum==k hai to hum kr sakte hai
         for(int i=0;i<size;i++)
         {
             sum+=nums[i];
-            if(sum==k)
-            {
-                count++;
-            }
+            // if(sum==k)
+            // {
+            //     count++;
+            // }
             int temp=sum-k;
             if(m.find(temp)!=m.end())
             {
