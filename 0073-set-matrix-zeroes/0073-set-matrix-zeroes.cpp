11d9cafc95aad82171107197d1ddab1a94adc1f3
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-06-25 10:51:00 +0530

    Time: 0 ms (100%), Space: 18.6 MB (77.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,56 +1,71 @@
 class Solution {
 public:
-    
- void setZeroes(vector<vector<int>> &arr) 
-{
-   int sr=arr.size();
-   int sc=arr[0].size();
-   int matrow=arr[0][0];
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        int sr = matrix.size();
+        int sc = matrix[0].size();
 
-   for(int i=0;i<sr;i++)
-   {
-      if(arr[i][0]==0)
-      {
-         matrow=0;
-      }
-       for(int j=1;j<sc;j++)
-       {
-          if(arr[i][j]==0)
-          {
-              arr[i][0]=0;
-              arr[0][j]=0;
-          }
-       }
-   }
+        int matcol = matrix[0][0]; // Used for first column flag
 
+        // Check if any element in the first row is 0
+        for(int i = 0; i < sc; i++)
+        {
+            if(matrix[0][i] == 0)
+            {
+                matcol = 0; // mark first row to be zeroed
+            }
+        }
 
-   for(int i=1;i<sr;i++)
-   {
-       for(int j=1;j<sc;j++)
-       {
-          if(arr[0][j]==0 || arr[i][0]==0)
-          {
-              arr[i][j]=0;
-          }
-       }
-   }
+        // Check if any element in the first column is 0
+        for(int i = 0; i < sr; i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                matrix[0][0] = 0; // mark first column to be zeroed
+            }
+        }
 
-    if(arr[0][0]==0)
-   {
-       for(int i=0;i<sc;i++)
-       {
-           arr[0][i]=0;
-       }
-   }
-   if(matrow==0)
-   {
-       for(int i=0;i<sr;i++)
-       {
-           arr[i][0]=0;
-       }
-   }
-   
+        // Mark rows and columns that need to be zeroed
+        for(int i = 1; i < sr; i++)
+        {
+            for(int j = 1; j < sc; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
 
-}
+        // Set the appropriate elements to 0 based on markers
+        for(int i = 1; i < sr; i++)
+        {
+            for(int j = 1; j < sc; j++)
+            {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
+        // Zero the first column if matrix[0][0] was marked
+        for(int i = 0; i < sr; i++)
+        {
+            if(matrix[0][0] == 0)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+        // Zero the first row if matcol was marked
+        for(int i = 0; i < sc; i++)
+        {
+            if(matcol == 0)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+    }
 };
