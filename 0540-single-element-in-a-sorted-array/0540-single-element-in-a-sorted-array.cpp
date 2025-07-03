22d935641b4c00ee40e3090375efa93382dac091
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-07-03 11:01:25 +0530

    Time: 0 ms (100%), Space: 26 MB (91.78%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,47 +2,39 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) 
     {
-        int size = nums.size();
-        int s = 0;
-        int e = size - 1;
-
-        while (s <= e)
+        int size=nums.size();
+        int s=0;
+        int e=size-1;
+        while(s<=e)
         {
-            int mid = s + (e - s) / 2;
-
-            // handle boundary cases
-            if (mid == 0 || mid == size - 1) 
+            int mid=s+(e-s)/2;
+             if (mid == 0 || mid == size - 1) 
                 return nums[mid];
 
-            // if the current element is different from both neighbors
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
             {
                 return nums[mid];
             }
-
-            // if current is same as next
-            if (nums[mid] == nums[mid + 1])
+            if(nums[mid]==nums[mid+1])
             {
-                if ((size - mid) % 2 == 0)
+                if((size-mid)%2==0)
                 {
-                    e = mid - 1;
+                   e=mid-1;
                 }
                 else
                 {
-                    s = mid + 2;
+                    s=mid+2;
                 }
             }
-
-            // if current is same as previous
-            else if (nums[mid] == nums[mid - 1])
+            if(nums[mid]==nums[mid-1])
             {
-                if ((mid - s + 1) % 2 == 0)
+                if((mid-s+1)%2==0)
                 {
-                    s = mid + 1;
+                   s=mid+1;
                 }
                 else
                 {
-                    e = mid - 2;
+                    e=mid-2;
                 }
             }
         }
