bfe06555d5ee5e2c7fd5b7f1e3163f90cc876271
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-07-09 15:15:12 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.9%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,12 +1,10 @@
-#include<string>
 class Solution {
 public:
     int myAtoi(string s) 
-    {
-        int len=s.length();
+    { 
+      int len=s.length();
         int i=0;
         int sign=1;
-        string ans="";
         while(i<len && s[i]==' ')
         {
             i++;
@@ -25,25 +23,20 @@ public:
         {
             i++;
         }
+        long long num=0;
         while(i<len && s[i]>='0' && s[i]<='9')
         {
-            ans+=s[i];
+            num=num*10+(s[i]-'0');
+            if(sign==1 && num>INT_MAX) 
+            {
+                return INT_MAX;
+            }
+            if(sign==-1 && num>INT_MAX) 
+            {
+                return INT_MIN;
+            }
             i++;
-        }
-        if(ans.empty())
-        {
-            return 0;
-        }
-        try 
-        {
-            long long val = stoll(ans) * sign;
-            if (val < INT_MIN) return INT_MIN;
-            if (val > INT_MAX) return INT_MAX;
-            return (int)val;
         } 
-        catch (const std::out_of_range&) 
-        {
-            return sign == 1 ? INT_MAX : INT_MIN;
-        }
+        return (int)(num*sign); 
     }
 };
