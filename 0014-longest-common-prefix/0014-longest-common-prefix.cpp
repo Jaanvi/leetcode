8d00d1aabfba868fe8575fc80f8441cb554424e9
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2025-08-19 03:09:41 +0530

    Time: 23 ms (5.1%), Space: 20.9 MB (5.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,102 @@
-#include<string>
+class TrieNode
+{
+  public:
+   char data;
+   TrieNode* child[26];
+   int childcount;
+   bool isterminal;
+
+   TrieNode(char ch)
+   {
+     data=ch;
+     for(int i=0;i<26;i++)
+     {
+        child[i]=NULL;
+     }
+     childcount=0;
+     isterminal=false;
+   }
+};
+
+class Trie
+{
+  public:
+  TrieNode* root;
+
+  Trie()
+  {
+    root=new TrieNode('\0');
+  }
+
+  void insertUtil(TrieNode* root,string word)
+  {
+     //base condition
+     if(word.length()==0)
+     {
+        root->isterminal=true;
+        return;
+     }
+
+     int index=word[0]-'a';
+     TrieNode* children;
+
+     if(root->child[index]!=NULL)
+     {
+        children=root->child[index];
+     }
+     else
+     {
+        //absent
+        children=new TrieNode(word[0]);
+        root->childcount++;
+        root->child[index]=children;
+     }
+     //recursion call
+    return  insertUtil(children,word.substr(1));
+     
+  }
+  void insert(string word)
+  {
+     insertUtil(root,word);
+  }
+
+  void longestprfx(string str,string &ans)
+  {
+    TrieNode* curr = root;
+
+        for (int i = 0; i < str.length(); i++) 
+        {
+            if (curr->childcount == 1 && !curr->isterminal) 
+            {
+                ans.push_back(str[i]);
+                int index = str[i] - 'a';
+                curr = curr->child[index];
+            } 
+            else
+            {
+                break; // divergence or terminal found
+            }
+
+            if (curr->isterminal) {
+                break;
+            }
+        }
+    }
+};
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) 
     {
-        string prefix=strs[0];
-        int size=strs.size();
-        for(int i=0;i<size;i++)
-        {
-            string curstr=strs[i];
-            string temprefix="";
-            for(int j=0;j<min(curstr.length(),prefix.length());j++)
-            {
-                if(prefix[j]==curstr[j])
-                {
-                    temprefix=temprefix+curstr[j];
-                } 
-                else
-                {
-                    break;
-                }
-            }  
-            prefix=temprefix;
-            if (prefix.empty()) break; // Exit early if no common prefix
-        }
-        return prefix;
+       int size=strs.size();
+       Trie t;
+       for(int i=0;i<size;i++)
+       {
+          t.insert(strs[i]);
+       } 
+       string ans = "";
+        // Use first word as reference
+        t.longestprfx(strs[0], ans);
+
+        return ans;
     }
 };
